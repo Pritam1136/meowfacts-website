946cf8842c883a11c7e29e947eadaba8ed74ceae
Pritam Roy <pritamroy1136@gmail.com>
2023-06-12 20:09:41 +0530

    updated footer

--- app/components/footer/footer.jsx
+++ app/components/footer/footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="bg-slate-900 pt-10 text-center pb-4 text-white">
-      Made with by Pritam and <a href="">open source community</a>.
+      Made with ❤️ by <a href="https://github.com/Pritam1136" className="text-indigo-500">Pritam</a>  and <a href="https://github.com/wh-iterabb-it/meowfacts" className="text-indigo-500">open source community</a>.
     </div>
   );
 }
